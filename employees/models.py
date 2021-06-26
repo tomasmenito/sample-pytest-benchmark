e32d18a62f3259a41e9aae8bae3ef8d0e3215c0d
Tomas Menito <tomasmenito@gmail.com>
2021-06-26 19:47:40 -0300

    Model adjustments

--- employees/models.py
+++ employees/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Employee(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(null=False, max_length=100)
-    address = models.CharField(null=False, max_length=100)
-    phone_number = models.CharField(null=False, max_length=20)
+    address = models.TextField(null=False, max_length=500)
+    phone_number = models.CharField(null=False, max_length=50)
     salary = models.DecimalField(null=False, decimal_places=2, max_digits=6)
